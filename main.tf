36c9c1adfa09316b2ae0cffd137349b5d0bf7b65
root <root@jenkins.linux.com>
2018-07-07 11:04:49 +0200

    First Commit

---
+++ main.tf
@@ -0,0 +1,85 @@
+
+provider "aws" {
+  region = "${var.region}"
+}
+
+
+resource "aws_security_group" "terraform" {
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_launch_configuration" "terraform" {
+  image_id = "${var.ami}"
+  instance_type = "${var.instance_type}"
+
+  user_data =<<-EOF
+                 #!/bin/bash
+                 yum -y install httpd
+                 echo 'Website 01!' > /var/www/html/index.html
+                 systemctl restart httpd
+                 systemctl enable  httpd
+                 firewall-cmd --permanent --add-port=80/tcp
+                 firewall-cmd --reload
+                EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+
+resource "aws_autoscaling_group" "terraform-ASG" {
+  launch_configuration = "${aws_launch_configuration.terraform.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers = ["${aws_elb.terraform-elb.name}"]
+  health_check_type = "ELB"
+
+  max_size = 10
+  min_size = 2
+
+  tag {
+    key = "name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_availability_zones" "all" {}
+
+
+resource "aws_elb" "terraform-elb" {
+  name = "terraform-elb"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.terraform.id}"]
+
+  "listener" {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    interval = 30
+    target = "HTTP:80/"
+    timeout = 3
+    unhealthy_threshold = 2
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+
+variable "region" {
+  type = "string"
+  default = "us-west-1"
+}
+
+variable "ami" {
+  type = "string"
+  default = "ami-e0ba5c83"
+}
+
+variable "instance_type" {
+  type = "string"
+  default = "t2.micro"
+}
